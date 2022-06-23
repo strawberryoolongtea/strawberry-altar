82e0b0ead248edf824278593ec6a47936a16a947
strawberryoolongtea <strawberrycream@kakao.com>
2022-06-23 21:07:44 +0900

    Update : 🤞 성물 아이템 데이터 수정 및 Result 컴포넌트 생성

--- data/items.js
+++ data/items.js
@@ -3,10 +3,14 @@ const height = 120;
 
 export const items = [
   { src: "/item/amulet.svg", alt: "amulet", width, height },
+  { src: "/item/bell.svg", alt: "bell", width, height },
+  { src: "/item/crystal-ball.svg", alt: "crystal-ball", width, height },
   { src: "/item/crystals.svg", alt: "crystals", width, height },
   { src: "/item/cup.svg", alt: "cup", width, height },
   { src: "/item/goddess-idol.svg", alt: "goddess-idol", width, height },
+  { src: "/item/incense.svg", alt: "incense", width, height },
   { src: "/item/knife.svg", alt: "knife", width, height },
+  { src: "/item/offerings.svg", alt: "offerings", width, height },
   { src: "/item/oil.svg", alt: "oil", width, height },
   { src: "/item/potions.svg", alt: "potions", width, height },
   { src: "/item/rosemaries.svg", alt: "rosemaries", width, height },
